d4a6d5e10393fd56e354210893e165f114fcbe2a
shijoshaji <jojovms@gmail.com>
2021-11-10 14:12:56 +0530

    Update settings.py

--- dj_resume/settings.py
+++ dj_resume/settings.py
@@ -147,6 +147,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'media')
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
